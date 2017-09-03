e1c49b415b983bc42bbb6cd9541c7d5d8b3211b9
gaoyong <gaoyong06@qq.com>
2017-09-03 15:53:44 +0800

    debug

--- backend/config/params.php
+++ backend/config/params.php
@@ -4,7 +4,8 @@ return [
 
     //http://www.yiichina.com/doc/guide/2.0/security-authorization
     //容许访问后台的uid列表
-    'adminUidWhiteList' => [10074],
+    'adminUidWhiteList' => [10001],
+
     //容许访问后台的ip列表
     'adminIps' => [],//'127.0.*'
 
